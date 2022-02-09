88033a22aa73ca8b8e1c4e513ac3037065b9ea3e
refigo <wlguq100@naver.com>
2022-02-09 12:49:09 +0900

    refact: rotate_stack and push_from_to

--- src/main.c
+++ src/main.c
@@ -6,67 +6,68 @@
 /*   By: mgo <mgo@student.42seoul.kr>               +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/02/01 13:05:46 by mgo               #+#    #+#             */
-/*   Updated: 2022/02/09 12:37:21 by mgo              ###   ########.fr       */
+/*   Updated: 2022/02/09 12:48:15 by mgo              ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "push_swap.h"
 
-void	push_from_to(t_stack **from_stack, t_stack **to_stack)
+// todo: no 2ptr
+void	push_from_to(t_stack *from_stack, t_stack *to_stack)
 {
 	t_dbly_lnkd	*moving;
 
-	if ((*from_stack)->top == NULL)
+	if ((from_stack)->top == NULL)
 		return ;
-	moving = (*from_stack)->top;
-	(*from_stack)->top = moving->next;
-	if ((*from_stack)->top)
-		(*from_stack)->top->prev = NULL;
+	moving = (from_stack)->top;
+	(from_stack)->top = moving->next;
+	if ((from_stack)->top)
+		(from_stack)->top->prev = NULL;
 	else
-		(*from_stack)->bot = NULL;
-	if ((*to_stack)->top)
-		(*to_stack)->top->prev = moving;
+		(from_stack)->bot = NULL;
+	if ((to_stack)->top)
+		(to_stack)->top->prev = moving;
 	else
-		(*to_stack)->bot = moving;
-	moving->next = (*to_stack)->top;
-	(*to_stack)->top = moving;
+		(to_stack)->bot = moving;
+	moving->next = (to_stack)->top;
+	(to_stack)->top = moving;
 }
 
-void	rotate_stack(t_dbly_lnkd **stack_top, t_dbly_lnkd **stack_bot)
+void	rotate_stack(t_stack *stack)
 {
 	t_dbly_lnkd	*moving;
 
-	if (*stack_top == NULL || (*stack_top) == (*stack_bot))
+	if (stack->top == NULL || stack->top == stack->bot)
 		return ;
-	moving = *stack_top;
-	*stack_top = moving->next;
-	(*stack_top)->prev = NULL;
-	(*stack_bot)->next = moving;
-	moving->prev = *stack_bot;
+	moving = stack->top;
+	stack->top = moving->next;
+	stack->top->prev = NULL;
+	stack->bot->next = moving;
+	moving->prev = stack->bot;
 	moving->next = NULL;
-	*stack_bot = moving;
+	stack->bot = moving;
 }
 
 void	sort_stack(t_push_swap *data)
 {
-	push_from_to(&(data->a), &(data->b));
-	push_from_to(&(data->a), &(data->b));
-	push_from_to(&(data->a), &(data->b));
+	push_from_to((data->a), (data->b));
+	push_from_to((data->a), (data->b));
+	push_from_to((data->a), (data->b));
 
-	ft_putendl_fd("data->a: ", 1);
+	ft_putendl_fd("[[data->a]]", 1);
 	test_t_stack(data->a);
-	ft_putendl_fd("data->b: ", 1);
+	ft_putendl_fd("[[data->b]]", 1);
 	test_t_stack(data->b);
 
-	/*
-	rotate_stack(&(data->a_top), &(data->a_bot));
-	//rotate_stack(&(data->b_top), &(data->b_bot));
+	rotate_stack(data->a);
+	rotate_stack(data->b);
+	rotate_stack(data->a);
+	rotate_stack(data->b);
 
-	ft_putendl_fd("data->a: ", 1);
+	ft_putendl_fd("[[data->a]]", 1);
 	test_t_stack(data->a);
-	ft_putendl_fd("data->b: ", 1);
+	ft_putendl_fd("[[data->b]]", 1);
 	test_t_stack(data->b);
-	*/
 }
 
 int	main(int argc, char **argv)
