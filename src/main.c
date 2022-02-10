c260cc6c59cd315cd85140c0a430d660fe8e5414
refigo <wlguq100@naver.com>
2022-02-10 10:14:00 +0900

    feat: get_stack_size and check_sorted_size

--- src/main.c
+++ src/main.c
@@ -6,15 +6,48 @@
 /*   By: mgo <mgo@student.42seoul.kr>               +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/02/01 13:05:46 by mgo               #+#    #+#             */
-/*   Updated: 2022/02/09 15:29:27 by mgo              ###   ########.fr       */
+/*   Updated: 2022/02/10 10:13:09 by mgo              ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "push_swap.h"
 
+int	get_stack_size(t_stack *stack)
+{
+	int			ret_size;
+	t_dbly_lnkd	*tmp;
+
+	ret_size = 0;
+	tmp = stack->top;
+	while (tmp)
+	{
+		ret_size++;
+		tmp = tmp->next;
+	}
+	printf("ret_size: [%d]\n", ret_size);
+	return (ret_size);
+}
+
+int	check_sorted_size(t_stack *stack, int size)
+{
+	t_dbly_lnkd	*tmp;
+
+	tmp = stack->top;
+	while (tmp->next && (--size))
+	{
+		if ((tmp->num) > (tmp->next->num))
+			return (false);
+		tmp = tmp->next;
+	}
+	return (true);
+}
+
 void	sort_stack(t_push_swap *data)
 {
-
+	if (check_sorted_size(data->a, get_stack_size(data->a)))
+		printf("sorted!\n");
+	else
+		printf("not sorted..\n");
 }
 
 int	main(int argc, char **argv)
